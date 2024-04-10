b7c910df4bb2296ede9f1c75287ef7b6d6d24797
Alias <93505057+aliassheikh@users.noreply.github.com>
2024-04-10 10:15:14 +0200

    Update DepositProperties.java (expand `import *`)

--- src/main/java/nl/knaw/dans/managedeposit/core/DepositProperties.java
+++ src/main/java/nl/knaw/dans/managedeposit/core/DepositProperties.java
@@ -17,7 +17,11 @@ package nl.knaw.dans.managedeposit.core;
 
 import nl.knaw.dans.managedeposit.core.service.TextTruncation;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import java.time.OffsetDateTime;
 
 @Entity
